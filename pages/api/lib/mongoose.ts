6275052f089cb561cb62beaa1c1c8cb992572772
Juan Fernando Cogollo Doria <cocodrilette@gmail.com>
2023-11-04 11:09:00 -0500

    Add Tailwind CSS and PostCSS dependencies, and Mongoose dependency for database management

---
+++ pages/api/lib/mongoose.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export const Schema = mongoose.Schema;
+export const ObjectId = Schema.ObjectId;
+export const model = mongoose.model.bind(mongoose);
+
+export const connect = async () => {
+  let db;
+
+  const connectionUrl = process.env.NEXT_PUBLIC_MONGODB_URI ?? "";
+
+  if (!db) {
+    db = await mongoose.connect(connectionUrl);
+    return db;
+  } else {
+    return db;
+  }
+};
---
+++ pages/api/lib/schemas/user.schema.ts
@@ -0,0 +1,76 @@
+import { Schema, model } from "../mongoose";
+
+interface IUser {
+  address: string;
+  name: string;
+  legalId: string;
+  email: string;
+  password: string;
+}
+
+interface UserModelInterface extends IUser {}
+
+const UserSchema = new Schema<UserModelInterface>({
+  address: String,
+  name: String,
+  legalId: String,
+  email: String,
+  password: String,
+});
+
+export class User {
+  private UserModel = model<UserModelInterface>("User", UserSchema);
+
+  async createUser(user: IUser) {
+    const newUser = new this.UserModel(user);
+    try {
+      const savedUser = await newUser.save();
+      console.log("User saved successfully!");
+      return savedUser;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async getAllUsers() {
+    try {
+      const users = await this.UserModel.find({});
+      console.log(users);
+      return users;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async updateUser(userId: string, user: Partial<IUser>) {
+    try {
+      const updatedUser = await this.UserModel.findByIdAndUpdate(userId, user, {
+        new: true,
+      });
+      console.log("User updated successfully!");
+      return updatedUser;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async deleteUser(userId: string) {
+    try {
+      const deletedUser = await this.UserModel.findByIdAndRemove(userId);
+      console.log("User deleted successfully!");
+      return deletedUser;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async findOneByAddress(address: string) {
+    try {
+      const user = await this.UserModel.findOne({ address });
+      console.log(user);
+      return user;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
