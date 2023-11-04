31a0d0be5a145ba1fa47e91905d9bcc24f4c3a56
JUAN CAMILO VELEZ CHAVERRA <juancamilotareas@gmail.com>
2023-11-04 13:59:31 -0500

    commit de velez pa las jajas

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
           href="/auth/register"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center "
         >
-          Get started
+          Get Started Now!
           <svg
             className="w-3.5 h-3.5 ml-2"
             aria-hidden="true"
